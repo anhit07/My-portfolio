865223c98467e652950ca4369d99938589f17ac3
VAIO\user <anhit07@gmail.com>
2017-05-25 16:15:03 -0400

    edit  view

--- public/angular_app/contact/contact_controller.js
+++ public/angular_app/contact/contact_controller.js
@@ -1,30 +1,34 @@
-angular.module('myPortfolioApp').controller('ContactController', ContactController);
+angular.module('myPortfolioApp').controller('ContactController',
+		ContactController);
 
 function ContactController($http, $timeout) {
 	var vm = this;
 
 	vm.sendMessage = function() {
 		var messageContent = {
-			senderName: vm.name,
-			senderEmail: vm.email,
-			subject: vm.subject,
-			message: vm.messageContent
+			senderName : vm.name,
+			senderEmail : vm.email,
+			subject : vm.subject,
+			message : vm.messageContent
 		};
 
 		$http.post('/api/sendmail', messageContent).then(function(result){
-			vm.message = 'Sent';
-			vm.error = '';
+			vm.message = 'Sent'; 
+			vm.error = ''; 
 		}).catch(function(error){
-			vm.error = 'Error'
-			vm.message = '';
-			console.log(error);
+			vm.error = 'Error';
+			vm.message = ''; 
+			console.log(error); 
 		});
 
 		$timeout(function() {
-    		vm.message = "";
-    		vm.error = ""
-	    }, 2500);
-    };
+			vm.message = '';
+			vm.error = '';
+		}, 2500);
+	};
+
+	vm.closeMessge = function() {
+		vm.message = '';
+		vm.error = '';
+	}
 }
-
-
